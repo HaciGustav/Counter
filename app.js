717c5788e2d0c9692986aa66bb67639d0cb6c005
MehmetFK <mehmetfkuruldak@gmail.com>
2022-09-13 10:28:55 +0200

    event added

---
+++ app.js
@@ -0,0 +1,32 @@
+//initial count
+let count = 0
+
+//select value and buttons
+const value = document.querySelector("#value")
+const buttons = document.querySelectorAll(".btn")
+console.log(value, buttons)  
+
+buttons.forEach((btn) => {
+    console.log(btn)
+    btn.addEventListener("click", (event) => {
+        const styles = event.currentTarget.classList
+        if(styles.contains("increase")){
+            count++
+        }else if(styles.contains("decrease")){
+            count--
+        }else{
+            count = 0
+        }
+        if(count < 0){
+            value.style.color = ("red")
+        }else if(count > 0){
+            value.style.color = ("green")
+        }else{
+            value.style.color = ("black")
+            
+        }
+        value.textContent = count
+        
+    })
+    
+})
